3e816af76f65a11d628c0c668c881765fa4b72ef
alqrnyahmd266-ship-it <alqrnyahmd266@gmail.com>
2026-03-11 16:01:10 +0300

    Update script.lua

--- script.lua
+++ script.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://pastebin.com/raw/TfnBTU3t"))()
+loadstring(game:HttpGet("https://msuelcjkfclhrnwuywte.supabase.co/functions/v1/get-paste?id=365a0a78-43c5-4203-86f6-9dda030ab647"))()
